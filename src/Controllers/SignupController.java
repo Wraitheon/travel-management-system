d5e20bb851d527d3bfe7dbf0d971e9a91d0f0278
Tayyaab Ali Sajid <tayyaabsajid@gmail.com>
2023-11-30 02:49:59 +0500

    signup page is pretty much done, only linking with login page left

--- src/Controllers/SignupController.java
+++ src/Controllers/SignupController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import javafx.util.StringConverter;
 
@@ -18,6 +17,9 @@ public class SignupController {
     private VBox signupBox;
 
     @FXML
+    private ComboBox<String> userTypeComboBox;
+
+    @FXML
     private TextField emailField;
 
     @FXML
@@ -28,9 +30,6 @@ public class SignupController {
 
     @FXML
     private DatePicker dobDatePicker;
-
-    @FXML
-    private TextField userTypeField;
 
     @FXML
     private TextField cnicField;
@@ -47,39 +46,100 @@ public class SignupController {
     @FXML
     private Hyperlink loginLink;
 
-    @FXML
-    private void initialize() {
+    public void initialize() {
         dobDatePicker.setConverter(new StringConverter<>() {
             @Override
             public String toString(LocalDate date) {
                 return (date != null) ? date.toString() : "";
             }
-
+    
             @Override
             public LocalDate fromString(String string) {
                 return (string != null && !string.isEmpty()) ? LocalDate.parse(string) : null;
             }
         });
+    
+        // Set the initial visibility of fields
+        emailField.setVisible(true);
+        nameField.setVisible(true);
+        ageField.setVisible(true);
+        dobDatePicker.setVisible(true);
+        cnicField.setVisible(true);
+        phoneField.setVisible(true);
+        passwordField.setVisible(true);
+    
+        // Register an event handler to handle user type changes in real time
+        userTypeComboBox.setOnAction(event -> handleUserTypeChange());
+    
+        // Set up a listener for changes in the dobDatePicker value
+        dobDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                int calculatedAge = calculateAge(newValue);
+                ageField.setText(String.valueOf(calculatedAge));
+            }
+        });
+    
+        // Make the ageField uneditable
+        ageField.setEditable(false);
     }
 
     @FXML
     private void handleSignupButtonAction(ActionEvent event) {
+        // Mandatory fields
         String email = emailField.getText();
         String name = nameField.getText();
-        int age = Integer.parseInt(ageField.getText());
-        String dateOfBirth = dobDatePicker.getValue().toString();
-        String userType = userTypeField.getText();
-        String cnic = cnicField.getText();
         String phoneNumber = phoneField.getText();
         String password = passwordField.getText();
 
-        dbhandler dbH = new dbhandler();
+        // Check if mandatory fields are empty
+        if (email.isEmpty() || name.isEmpty() || phoneNumber.isEmpty() || password.isEmpty()) {
+            // Show an alert for missing mandatory fields
+            showAlert("Mandatory Fields", "Please fill in all mandatory fields.");
+            return;
+        }
 
-        dbH.insertUser(email, name, age, dateOfBirth, userType, cnic, phoneNumber, password);
+        // Optional fields
+        LocalDate dateOfBirth = dobDatePicker.getValue();
+        Integer age = (dateOfBirth != null) ? calculateAge(dateOfBirth) : null;
+        String userType = userTypeComboBox.getValue();
+        String cnic = cnicField.getText();
+
+        // Set optional fields to null if empty
+        cnic = (cnic.isEmpty()) ? null : cnic;
+        age = (ageField.getText().isEmpty()) ? null : age;
+        dateOfBirth = (dobDatePicker.getValue() == null) ? null : dateOfBirth;
+
+        dbhandler dbH = new dbhandler();
+        dbH.insertUser(email, name, age, (dateOfBirth != null) ? dateOfBirth.toString() : null, userType, cnic, phoneNumber, password);
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private int calculateAge(LocalDate birthdate) {
+        LocalDate currentDate = LocalDate.now();
+        return currentDate.getYear() - birthdate.getYear();
     }
 
     @FXML
     private void handleLoginLinkAction() {
         // Implement the logic to navigate to the login page
+    }
+
+    @FXML
+    private void handleUserTypeChange() {
+        // Check the selected user type and adjust field visibility accordingly
+        String selectedUserType = userTypeComboBox.getValue();
+
+        boolean isTravelAgency = "Travel Agency".equals(selectedUserType);
+
+        ageField.setVisible(!isTravelAgency);
+        dobDatePicker.setVisible(!isTravelAgency);
+        cnicField.setVisible(!isTravelAgency);
     }
 }
